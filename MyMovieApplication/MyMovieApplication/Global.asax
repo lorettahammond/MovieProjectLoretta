cad96b0c0b853aea43fb0177a0d0e3264503eb94
lorettahammond <lhammo09@baker.edu>
2014-07-01 14:15:37 -0400

    MyMovieApplication
    Created movie app from handouts on 7.1.14

---
+++ MyMovieApplication/MyMovieApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMovieApplication.MvcApplication" Language="C#" %>
---
+++ MyMovieApplication/MyMovieApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMovieApplication.Startup))]
+namespace MyMovieApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
