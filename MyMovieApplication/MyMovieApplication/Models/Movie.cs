3036630ce405a83655a444a0b7d9c8c56f4e4334
lorettahammond <lhammo09@baker.edu>
2014-07-01 15:53:56 -0400

    My Movie Application
    Attempting to add Director as a field

--- MyMovieApplication/MyMovieApplication/Models/Movie.cs
+++ MyMovieApplication/MyMovieApplication/Models/Movie.cs
@@ -13,6 +13,7 @@ namespace MyMovieApplication.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price {get; set;}
+        public string Director { get; set; }
     }
     public class MovieDBContext : DbContext 
     {
